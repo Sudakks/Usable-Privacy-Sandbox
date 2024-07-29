326df8565fdbb3116da8918bd971002bc61b923e
Sudakks <627936779@qq.com>
2024-07-30 05:58:07 +0800

    test row

--- basicInfo_page/basicInfo.js
+++ basicInfo_page/basicInfo.js
@@ -4,3 +4,41 @@ document.addEventListener("DOMContentLoaded", function () {
         window.location.href = "../popup.html";
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // 事件委托
+    document.body.addEventListener('click', function (event) {
+        // 判断是否点击了可编辑的元素
+        if (event.target.classList.contains('infoEdit')) {
+            var infoDiv = event.target;
+            var input = infoDiv.nextElementSibling;
+
+            input.value = infoDiv.textContent.trim();
+            infoDiv.style.display = 'none';
+            input.style.display = 'inline';
+            input.focus();
+        }
+    });
+
+    document.body.addEventListener('blur', function (event) {
+        // 判断是否失去了输入框的焦点
+        if (event.target.classList.contains('infoInput')) {
+            var input = event.target;
+            var infoDiv = input.previousElementSibling;
+            var newValue = input.value.trim();
+
+            if (newValue !== "") {
+                infoDiv.textContent = newValue;
+            }
+
+            infoDiv.style.display = 'inline';
+            input.style.display = 'none';
+        }
+    }, true); // 捕获阶段
+
+    document.body.addEventListener('keypress', function (event) {
+        if (event.target.classList.contains('infoInput') && event.key === 'Enter') {
+            event.target.blur(); // 触发blur事件
+        }
+    }); 
+});
