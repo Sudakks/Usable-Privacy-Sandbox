51d8e734ebf9a9ee8a4b124d7770b217d90cdffd
Sudakks <627936779@qq.com>
2024-08-10 00:02:31 +0800

    add popup for overview

--- frontend/overview_page/overview.js
+++ frontend/overview_page/overview.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
     // 处理返回按钮的点击事件
     var backButton = document.querySelector(".backButton");
+    var backModal = document.getElementById("backModal");
     backButton.addEventListener("click", function () {
-        window.location.href = "../popup.html";
+        backModal.style.display = "block";
+        //window.location.href = "../popup.html";
     });
 
     // 从 localStorage 中获取 persona 数据
@@ -36,15 +38,112 @@ document.addEventListener("DOMContentLoaded", function () {
         console.error('No persona data found in localStorage');
     }
 
-    var modal = document.getElementById("myModal");
+    var saveModal = document.getElementById("saveModal");
     var saveBtn = document.querySelector(".saveButton");
-    var close = document.querySelector(".close");
+    var close = document.querySelectorAll(".close");
+    var discardBtn = document.querySelector(".discardButton");
+    var discardModal = document.getElementById("discardModal");
 
-    saveBtn.onclick = function () {
-        modal.style.display = "block";
-    }
-    close.addEventListener("click", () => {
-        modal.style.display = "none";
+    saveBtn.addEventListener("click", function () {
+        saveModal.style.display = "block";
+    });
+    discardBtn.addEventListener("click", function () {
+        discardModal.style.display = "block";
+    });
+    close.forEach((btn) => {
+        btn.addEventListener("click", () => {
+            if (discardModal.contains(btn)) {
+                discardModal.style.display = "none";
+            }
+            else if (saveModal.contains(btn)) {
+                saveModal.style.display = "none";
+            }
+            else if (backModal.contains(btn)) {
+                backModal.style.display = "none";
+            }
+            else {
+                console.error('Couldn\'t find close button');
+            }
+        });
     });
 });
+
+
+$(document).ready(function () {
+    function showPopupMessage(message) {
+        $('#popupMessage').text(message).fadeIn();
+
+        setTimeout(function () {
+            $('#popupMessage').fadeOut();
+            $('#saveModal').fadeOut();
+        }, 1000); // Message shows for 1.5 seconds
+    }
+
+    function showPopupMessageAndBack(message) {
+        $('#popupMessage').text(message).fadeIn();
+
+        setTimeout(function () {
+            $('#popupMessage').fadeOut();
+            $('#backModal').fadeOut();
+
+            // 在popupMessage展示后0s立即跳转popup.html
+            setTimeout(function () {
+                window.location.href = "../popup.html";
+            }, 0);
+
+        }, 1000);  // 显示 #popupMessage 的1s时间
+    }
+
+
+    function onlyExit() {
+        $('#discardModal').fadeOut();
+    }
+
+    $('#saveUpdate').click(function () {
+        showPopupMessage('Save & Update Successful!');
+    });
+
+    $('#saveNoUpdate').click(function () {
+        showPopupMessage('Only Saved Changes!');
+    });
+
+    $('#Cancel').click(function () {
+        onlyExit();
+    });
+
+    $('#Confirm').click(function () {
+        onlyExit();
+    });
+
+    /*
+    $('.close').click(function () {
+        $('#saveModal').fadeOut();
+    });
+    */
+
+    /* process checkbox */
+    var confirmBack = document.getElementById("confirmBack");
+    confirmBack.addEventListener("click", () => {
+        var operation = null;
+        var obj = document.getElementsByName("backOption")
+        for (var i = 0; i < obj.length; i++) { //遍历Radio 
+            if (obj[i].checked) {
+                operation = obj[i].value;
+                break;
+            }
+        }
+
+        if (operation === "save-only") {
+            showPopupMessageAndBack('Only Saved Changes!');
+        }
+        else if (operation === "save-update") {
+            showPopupMessageAndBack('Save & Update Successful!');
+        }
+        else if (operation === "discard") {
+            //onlyExit();
+            window.location.href = "../popup.html";
+        }
+    });   
+});
+
 
